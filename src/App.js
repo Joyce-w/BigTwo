de8e800a71059a7c18c968fc2766d6711aced1c4
Joyce <hellojoycewong@gmail.com>
2021-07-31 10:48:56 -0700

    added current player

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
-import BigTwo from './gameLogic/game';
-import PlayerCount from './PlayerCount';
+import BigTwo from './game';
 
 function App() {
-
-  /**Sets the player count to state based on handlePlayers()*/
+  /*Save deckID */
+  const [deckID, setDeckID] = useState();
+  /*Start with 2 players*/
   const [players, setPlayers] = useState(2);
-  const [deck_id, setDeck_id] = useState(2);
+  /*Split deck of cards amongst two players */
+  const [playerOne, setPlayerOne] = useState();
+  const [playerTwo, setPlayerTwo] = useState();
+  /*Keep track of which player is currently playing */
+  const [isPlayerOne, setIsPlayerOne] = useState(true)
+  /**Keep track of the most curent play */
+  const initial_play = { numCardsPlayed: 0, cards: []}
+  const [currPlay, setCurrPlay] = useState(initial_play)
 
-  //set state so all player cards are stored
+  //get new deck from api
+  useEffect(() => {
+    const newDeck = async() => {
+      let {deck_id} = await BigTwo.shuffleNewCard();
+      setDeckID(deck_id);
+    }
+    newDeck();
+  },[])
+  
+  /*split deck when a new deckID is obtained*/
+  useEffect(() => {
+    //set state so all player cards are stored
+    const splitDeck = async () => {
+      let {p1, p2} = await BigTwo.splitDeck(players, deckID)
+      setPlayerOne(p1.data.cards)
+      setPlayerTwo(p2.data.cards)
+    }
+    
+    splitDeck();
+    ;
+  },[deckID])
 
   //set state to show current player
   //set state to show currentPlay
 
   
-  console.log(BigTwo.splitDeck(2, "ptwiqsnljtgr"))
   return (
     <div className="App">
-      <PlayerCount handlePlayers={handlePlayers}/>
     </div>
   );
 }
