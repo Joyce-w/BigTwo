20f42c967fe0a7a4d55c83b0523ed9a0e8081304
Joyce <hellojoycewong@gmail.com>
2021-08-01 13:55:56 -0700

    check 3Kind, fullHouse, 4Kind

--- src/CardCheck.js
+++ src/CardCheck.js
@@ -15,7 +15,7 @@ class CardCombos {
         else if (currPlayCards !== 0 && newHandCount === currPlayCards) {
             console.log('they are the same number of cards not 0')
             return true;
-        }        
+        }
         else {
             return false;
         }
@@ -51,22 +51,56 @@ class CardCombos {
     /**Handles 5 card play */
     static isValidFiveCard = (hand) => {
         //goes into pokerhands
+        let cardValues = hand.map(card => PokerHands.numOrder[card.value]).sort((a, b) => a - b);
 
         //check if cards are a flush
-        let isFlush = hand.map(card => card.suit).every((suit,idx,arr) => suit === arr[0])
+        let isFlush = hand.map(card => card.suit).every((suit, idx, arr) => suit === arr[0])
         
         //check for straight
-        let isSequential = hand.map(card => PokerHands.numOrder[card.value]).sort((a, b) => a - b)
+        let isSequential = cardValues
             .every((el, i, arr) => i === arr.length - 1 || el + 1 === arr[i + 1])
         
         //check for straight flush 
         let isStraightFlush = isFlush && isSequential ? true : false;
 
+        //check for 4 of a kind
+        const isFourOfAKind = (hand) => {
+            let count = {};
+            for (let val of cardValues) {
+                if (count[val]) {
+                    count[val] += 1;
+                } else {
+                    count[val] = 1;
+                }
+            }
+            console.log('count', count)
+            if (Object.values(count).includes(4)) {
+                console.log('there are 4 cards of same value')
+                return true;
+            }
+        }
 
-        return(isStraightFlush)
+        //check for 3 card combos (full house or 3 of a kind)
+        const isThreeCard = (hand) => {
+            let count = {};
+            for (let val of cardValues) {
+                if (count[val]) {
+                    count[val] += 1;
+                } else {
+                    count[val] = 1;
+                }
+            }
+            console.log('count', count)
+            let arraySum = Object.values(count).sort((a, b) => a - b).join('');
+            // 113 is 3 of a kind & 23 is full house
+            let res = arraySum === '113' || arraySum === '23' ? true : false;
+            return res
+
+        }
+
+        console.log(isThreeCard(hand))
     }
 }
-
 
     
 export default CardCombos;
