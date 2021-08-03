ff0937e64b05a3140d8b256185428e4d978d8556
Joyce <hellojoycewong@gmail.com>
2021-08-03 01:52:51 -0700

    add button

--- src/LandingPg.js
+++ src/LandingPg.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './LandingPg.css'
-
+import { Link } from 'react-router-dom';
 function LandingPg() {
 
 
@@ -12,18 +12,18 @@ function LandingPg() {
                     <li><b>Objective:</b>
                         <br></br>
                         Be the first player to play all your cards.</li>
-                    <br></br>
+                    
 
                     <li><b>How to play:</b>
                         <br></br>
                         Player one starts by choosing a combination. Once played, player two must choose the same number of cards that was previously played
                         and beat the current hand displayed. Pressing pass will reset the current hand and the next player decides on the new combination.</li>
-                    <br></br>
+                    
                     
                     <li><b>Combinations:</b>
                         <br></br>
                         Players can only play a single card, pair, or 5 card combination. Anything else is invalid!</li>
-                    <br></br>
+                    
 
                     <li><b>Rankings:</b>
                         <br></br>
@@ -31,15 +31,17 @@ function LandingPg() {
                         <br></br>
                         (Lowest) ♦, ♣ , ♥, ♠ (Highest)
                     </li>
-                    <br></br>
+                    
                     {/* App information */}
                     <li>
                         <b>About This App:</b>
                         <br></br>
                         This app was built by <a href="https://www.linkedin.com/in/hello-joycewong/">Joyce</a> with <a href="https://reactjs.org/">React </a>
+                        <br></br>
+                        For <a href="https://mintbean.io/meets?sort=upcoming">Mintbean's</a> Hackathon!
                         
                     </li>
-                
+                    <button><Link to="/game">Start Game</Link></button>
                 </ul>
             </div>
       </div>
